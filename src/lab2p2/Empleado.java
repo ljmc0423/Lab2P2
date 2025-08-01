d235b591e627879373b67adf195b372554e5c2e6
Leonardo López <lopezvelasquezleonardodavid@gmail.com>
2025-08-01 09:38:54 -0600

    Empleado Ventas clase

--- src/lab2p2/Empleado.java
+++ src/lab2p2/Empleado.java
@@ -12,43 +12,39 @@ import java.util.Calendar;
  * @author ljmc2
  */
 public class Empleado {
-    
+
     protected int codigoEmpleado;
     protected String nombreEmpleado;
     protected Calendar fechaContratacion;
     protected double salarioBase;
     protected int horasTrabajadas;
-    
-    public Empleado(int codigo, String nombre, double salario){
-        codigoEmpleado=codigo;
-        nombreEmpleado=nombre;
-        fechaContratacion=Calendar.getInstance();
-        salarioBase=salario;
-        horasTrabajadas=0;
+
+    public Empleado(int codigo, String nombre, double salario) {
+        codigoEmpleado = codigo;
+        nombreEmpleado = nombre;
+        fechaContratacion = Calendar.getInstance();
+        salarioBase = salario;
+        horasTrabajadas = 0;
     }
-    
-    public void registrarHoras(int horas){
-        horasTrabajadas+=horas;
+
+    public void registrarHoras(int horas) {
+        horasTrabajadas += horas;
     }
-    /*
-    Cálculo de pago: Calcula el salario proporcional según las horas trabajadas, tomando un mes estándar de 160 horas como base.
-    Para empleados estándar, se aplica una deducción fija del 3.5%.
-    */
-    public double calcularPago(){
+
+    public double calcularPago() {
         double pago = (salarioBase * horasTrabajadas) / 160;
         return pago - (pago * 0.035);
     }
-    
+
     public String mostrarInfo() {
         int dia = fechaContratacion.get(Calendar.DAY_OF_MONTH);
-        int mes = fechaContratacion.get(Calendar.MONTH) + 1; // Mes empieza en 0
-        int year = fechaContratacion.get(Calendar.YEAR);
+        int mes = fechaContratacion.get(Calendar.MONTH) + 1;
+        int anno = fechaContratacion.get(Calendar.YEAR);
 
-        return "Código: " + codigoEmpleado +
-               "\nNombre: " + nombreEmpleado +
-               "\nFecha de contratación: "+dia+ "/" + mes + "/" +
-               year;
+        return "Código: " + codigoEmpleado
+                + "\nNombre: " + nombreEmpleado
+                + "\nFecha de contratación: " + dia + "/" + mes + "/"
+                + anno;
     }
-    
-    
+
 }
--- src/lab2p2/EmpleadoVentas.java
+++ src/lab2p2/EmpleadoVentas.java
@@ -4,10 +4,57 @@
  */
 package lab2p2;
 
+import java.util.Calendar;
+
 /**
  *
  * @author ljmc2
  */
-public class EmpleadoVentas {
+public class EmpleadoVentas extends Empleado {
+
+    private double ventasMensuales[];
+    private double tasaComision;
+    private int mesActual;
+
+    public EmpleadoVentas(int codigo, String nombre, double salario) {
+        super(codigo, nombre, salario);
+        this.ventasMensuales = new double[12];
+        this.tasaComision = 0.05;
+    }
+
+    public boolean registrarVenta(double monto) {
+        mesActual = Calendar.getInstance().get(Calendar.MONTH);
+        if (monto > 0) {
+            ventasMensuales[mesActual] += monto;
+            return true;
+        }
+        return false;
+    }
+
+    public double calculoComision() {
+        mesActual = Calendar.getInstance().get(Calendar.MONTH);
+        return ventasMensuales[mesActual] * tasaComision;
+    }
+
+    @Override
+
+    public double calcularPago() {
+        return super.calcularPago() + calculoComision();
+    }
+
+    public double totalVentas() {
+        double total = 0;
+        for (double i : ventasMensuales) {
+            total += i;
+        }
+
+        return total;
+    }
     
+    @Override
+    
+    public String mostrarInfo(){
+        return super.mostrarInfo() + "\nVentas Anuales: " + String.format("%.2f", totalVentas()) + " Lps";
+    }
+
 }
